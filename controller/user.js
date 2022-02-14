93ac121a2fab0cbe8e1a47606670426148386037
cid <cid.santiago1@gmail.com>
2022-02-14 18:30:39 -0500

    initial commit

---
+++ controller/user.js
@@ -0,0 +1,36 @@
+﻿import { v4 as uuidv4 } from 'uuid';
+
+let users = [];
+
+export const getUser = (req, res) => {
+    res.send(users);
+}
+export const createUser = (req, res) => {
+    const user = req.body;
+
+    users.push({ ... user, id: uuidv4() });
+    res.send(`User ${user.firstName} added to the database`);
+}
+export const getUserId = (req, res) => {
+    const { id } = req.params;
+    const foundUser = users.find((user) => user.id == id);
+
+    res.send(foundUser);
+}
+export const deleteUser = (req, res) => {
+    const {id} = req.params;
+
+    users = users.filter((user) => user.id != id );
+    res.send(`User with the id ${id} deleted` );
+}
+export const patchUser = (req, res) => {
+    const {id} = req.params;
+    const {firstName, lastName, age} = req.body;
+    const user = users.find((user) => user.id == id )
+    
+    if(firstName) user.firstName = firstName;
+    if(lastName) user.lastName = lastName;
+    if(age) user.age = age;
+    
+    res.send(`User with the id ${id} is updated`);
+}
---
+++ routes/users.js
@@ -0,0 +1,11 @@
+﻿import express from 'express';
+import { getUser, createUser, getUserId, deleteUser, patchUser } from '../controller/user.js';
+
+const router = express.Router();
+
+router.get('/', getUser);
+router.post('/', createUser);
+router.get('/:id', getUserId);
+router.delete('/:id', deleteUser);
+router.patch('/:id', patchUser);
+export default router;
